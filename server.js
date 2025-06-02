4e27a5eeb924cf7e8cbac5c1fbf5a0abf931844a
RochaS18 <igorrochasilva152020@gmail.com>
2025-06-02 20:22:00 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+const users = []
+
+
+app.post('/usuarios', (req, res) =>{} )
+
+app.get('/usuarios', (req, res) =>{} )
+
+
+app.listen(3000)
